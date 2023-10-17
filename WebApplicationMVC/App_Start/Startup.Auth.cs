9834c98528ddf8451c75b448873666f193159830
Wael Elkadim <elkadeem@hotmail.com>
2023-10-17 13:48:53 +0400

    Add MVC app with cookies authentication

---
+++ WebApplicationMVC/App_Start/Startup.Auth.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+namespace WebApplicationMVC
+{
+    public partial class Startup
+    {
+        private const string ApplicayionName = "sampleApp";
+        private const string CookieName = "sample.Cookies";
+        public static void ConfigureAuth(IAppBuilder app)
+        {
+
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions()
+            {                
+                CookieName = CookieName,
+                CookiePath = "/",
+                LoginPath = new PathString("/Account/Login"),
+                LogoutPath = new PathString("/Account/Logout"), 
+                ExpireTimeSpan = System.TimeSpan.FromMinutes(30),
+            });
+        }
+    }
+}
---
+++ WebApplicationMVC/Controllers/AccountController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationMVC.Controllers
+{
+    public class AccountController : Controller
+    {        
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+                return View(viewModel);
+
+            var claims = new List<Claim>{
+                    new Claim(ClaimTypes.Name, viewModel.UserName)
+                    , new Claim(ClaimTypes.Email, viewModel.UserName)
+                    , new Claim("NationId", "Id value from identity store")
+                    
+                };
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationType);
+            var authProperties = new AuthenticationProperties
+            {
+                IsPersistent = true,
+            };
+                        
+            Request.GetOwinContext()
+                .Authentication
+                .SignIn(authProperties
+                , claimsIdentity);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        // Logout action
+        public ActionResult Logout()
+        {
+            Request.GetOwinContext()
+                .Authentication
+                .SignOut(CookieAuthenticationDefaults.AuthenticationType);
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+
+    public class LoginViewModel
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
